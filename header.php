018b6f0eb80095421fedc65114a0b333b4228b76
Tiziana <95030727+TiziSil@users.noreply.github.com>
2023-03-25 12:28:31 +0100

    Barre de navigation

--- header.php
+++ header.php
@@ -1,26 +1,25 @@
 <header>
   <div class="py-2 text-bg">
-    <div class="container">
-      <div class="d-flex align-items-center">
-        <svg class="burger col-5">
-          <a href="#">
-            <image height="32px" fill="#DEC7B1" width="32px" href="assets\images\burger-solid.svg" />
+    <div class="container-fluid  text-center">
+      <div class="mx-3 d-flex align-items-center">
+        <div class="col">
+          <a href="#" class="burger-button">
+            <svg width="32px" height="32px">
+              <image height="32px" fill="#DEC7B1" width="32px" href="assets\images\burger-solid.svg" />
+            </svg>
           </a>
-        </svg>
+        </div>
 
+        <h1 class="col">Makisine</h1>
 
-        <button class="bouton-berger" type="button" aria-label="toggle curtain navigation">
-        </button>
+        <div class="col">
+          <a href="#" class="mon-compte-button" title="Mon compte">
+            <svg fill="white" class="d-block" width="32px" height="32px">
+              <use href="#people-circle" />
+            </svg>
+          </a>
+        </div>
 
-        <h1 class="col-5">Makisine</h1>
-
-
-        <a href="#" class="nav-link text-white">
-          <svg fill="white" class="d-block" width="40%" height="24">
-            <use href="#people-circle" />
-          </svg>
-          Mon compte
-        </a>
       </div>
     </div>
   </div>
