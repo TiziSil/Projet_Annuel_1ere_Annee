15a377182dfbd6720b17ebf1f10d6982d4ce84a1
Nine-rem <127841848+Nine-rem@users.noreply.github.com>
2023-05-03 19:15:29 +0200

    creation conf.inc.php

---
+++ core/userAdd.php
@@ -0,0 +1,152 @@
+<?php 
+session_start();
+require "functions.php";
+require "../pages/modale-inscription.php"
+
+//recuperer donnée
+//genre
+//nom
+//prenom
+//date de naissance
+//type de compte
+//mot de passe
+
+//vérification des données
+if (count($_POST)!=9
+    //||!isset($_POST["gender"])
+    || empty ($_POST["firstname"])
+    || empty ($_POST["lastname"])
+    || empty ($_POST["email"])
+    || empty ($_POST["pwd"])
+    || empty ($_POST["pwdConfirm"])
+    || empty ($_POST["birthday"])
+    || !isset ($_POST["newsletter"])
+    || empty ($_POST["cgu"])
+
+){
+    die ("Tentative de HACK");
+}
+
+//Nettoyage des données
+
+$gender = $_POST['gender'];
+$firstname = cleanFirstname($_POST['firstname']);
+$lastname = cleanLastname($_POST['lastname']);
+$email = cleanEmail($_POST['email']);
+$pwd = $_POST['pwd'];
+$pwdConfirm = $_POST['pwdConfirm'];
+$birthday = $_POST['birthday'];
+$account_type = $_POST["account_type"];*
+$newsletter = $_POST["newsletter"]
+$cgu = $_POST['cgu'];
+
+$listOfErrors = [];
+
+// --> Est-ce que le genre est cohérent
+$listGenders = [0,1,2];// on vérifie que c'est bien les valeurs pour éviter la faille xss
+if( !in_array($gender, $listGenders) ){
+	$listOfErrors[] = "Le genre n'existe pas";
+}
+// --> Nom plus de 2 caractères
+if(strlen($lastname) < 2){
+	$listOfErrors[] = "Le nom doit faire plus de 2 caractères";
+}
+
+// --> Prénom plus de 2 caractères
+// --> Nom plus de 2 caractères
+if(strlen($firstname) < 2){
+	$listOfErrors[] = "Le prénom doit faire plus de 2 caractères";
+}
+// --> Format de l'email
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+	$listOfErrors[] = "L'email est incorrect";
+}
+// --> Unicité de l'email (plus tard)
+$connection = connectDB();
+$queryPrepared = $connection ->prepare("SELECT * FROM makisine_utilisateur WHERE email = :email");
+//$queryPrepared = $connection->prepare("SELECT * FROM esgi_user WHERE email=:email");
+$queryPrepared-> execute([ "email" => $email ]);
+$results = $queryPrepared -> fetch();
+if (!empty($results)){
+	$listOfErrors[] = "L'email est déjà utilisé";
+}
+
+
+// --> Complexité du pwd
+if(strlen($pwd) < 8
+ || !preg_match("#[a-z]#", $pwd)
+ || !preg_match("#[A-Z]#", $pwd)
+ || !preg_match("#[0-9]#", $pwd)){
+	$listOfErrors[] = "Le mot de passe doit faire au min 8 caractères avec des minuscules, des majuscules et des chiffres";
+}
+
+
+// --> Meme mot de passe de confirmation
+if( $pwd != $pwdConfirm){
+	$listOfErrors[] = "La confirmation du mot de passe ne correspond pas";
+}
+// --> Est-ce que le pays est cohérent
+$listCountries = ["fr", "pl", "al", "be"];
+if( !in_array($country, $listCountries) ){
+	$listOfErrors[] = "Le pays n'existe pas";
+}
+
+
+// --> Date de naissance entre 6ans et 99ans
+
+//$birthday = "1986-11-29";
+
+$birthdayExploded = explode("-", $birthday);
+//Vérification de la date
+if (!checkdate($birthdayExploded[1],$birthdayExploded[2],$birthdayExploded[0])){
+	$listOfErrors[] = "Date de naissance incorrecte";
+}else{
+	//Vérification de l'age
+	$todaySecond = time();
+	$birthdaySecond = strtotime($birthday);
+	$ageSecond = $todaySecond - $birthdaySecond;
+	$age = $ageSecond/60/60/24/365.25;
+	if( $age <= 6 || $age >= 99 ){
+		$listOfErrors[] = "Vous n'avez pas l'âge requis (entre 6 et 99 ans)";
+	}
+}
+
+//Si OK
+if(empty($listOfErrors)){
+	//Insertion en BDD
+	// DSN permet une optimisation, USER, PWD
+	//pdo permet d'utiliser n'importe quel sgdb
+
+
+
+	$queryPrepared = $connection -> prepare("INSERT INTO makisine_UTILISATEUR
+															(gender, firstname, lastname, email, pwd, birthday, country)
+											VALUES
+															( :gender, :firstname, :lastname, :email, :pwd, :birthday, :country)") ;
+	$queryPrepared -> execute([
+								"gender" => $gender,
+								"firstname" => $firstname,
+								"lastname" => $lastname,
+								"email" => $email,
+								"pwd" => password_hash($pwd, PASSWORD_DEFAULT),
+								"birthday" => $birthday,
+								"country" => $country
+
+			
+	]);
+	
+	
+	//Redirection sur la page de connexion
+	header('location: ../home-page/tuile-1-accueil.php');
+}else{
+
+	//Si NOK
+	//On stock les erreurs et la data
+	$_SESSION['listOfErrors'] = $listOfErrors;
+	unset($_POST["pwd"]);
+	unset($_POST["pwdConfirm"]);
+	$_SESSION['data'] = $_POST;
+	//Redirection sur la page d'inscription
+	header('location: ../pages/modale-inscription.php');
+}
+
--- pages/modale-inscription.php
+++ pages/modale-inscription.php
@@ -1,3 +1,9 @@
+<?php
+//session_start();
+include "conf.inc.php";
+require "core/functions.php";
+?>
+
 <div id="modale-inscription" class="modale-inscription">
     <div class="modale-content-inscription">
         <div class="d-flex justify-content-between">
@@ -19,31 +25,62 @@
             </label>
             <span class="selection"></span>
         </div>
+        <?php if (isset($_SESSION["listOfErrors"])){ ?>
+            <div>
+                <div class = "alert alert-danger alert-dismissible fade show" role = "alert">
+                    <?php 
+                    foreach ($_SESSION['listOfErrors'] as $errors){
+                        echo "<li>" .$error ."</li>";
+                    }
+                    unset($_SESSION['listOfErrors']);
+                    ?>
+
+
+                    <button type = "button" class ="btn-close" data-bs-dismiss = "alert" aria-label = "Close"></button>
+                </div>
+            </div>
+        <?php } ?>    
+
         <div>
-            <form class="form">
+            <form class="form" action = "../core/userAdd.php" method = "POST" >
                 <div class="champ">
-                    <input type="text" class="input-champ" name="firstname" placeholder="Votre prénom" required="required" value="">
+                    <input type="text" class="input-champ" name="firstname" placeholder="Votre prénom" required="required" 
+                    value="<?= ( !empty($_SESSION["data"]))?$_SESSION["data"]["firstname"]:""; ?>">
+                    
                 </div>
+                
                 <div class="champ">
-                    <input type="text" class="input-champ" name="lastname" placeholder="Votre nom" required="required">
+                    <input type="text" class="input-champ" name="lastname" placeholder="Votre nom" required="required"
+                    value="<?= ( !empty($_SESSION["data"]))?$_SESSION["data"]["laststname"]:""; ?>">
                 </div>
+                
                 <div class="champ">
-                    <input type="email" class="input-champ" name="email" placeholder="Votre email" required="required">
+                    <input type="email" class="input-champ" name="email" placeholder="Votre email" required="required"
+                    value="<?= ( !empty($_SESSION["data"]))?$_SESSION["data"]["email"]:""; ?>">
                 </div>
+                
                 <div class="champ"><input type="password" class="input-champ" name="pwd" placeholder="Votre mot de passe" required="required"></div>
+               
                 <div class="champ"><input type="password" class="input-champ" name="pwdConfirm" placeholder="Confirmation" required="required"></div>
+                
                 <div class="champ"><select name="country" class="form-select">
-                        <option value="fr">France</option>
-                        <option value="pl">Pologne</option>
-                        <option value="al">Algérie</option>
-                        <option value="be">Belgique</option>
+                <option value="fr" <?= ( !empty($_SESSION["data"]) && $_SESSION["data"]["country"]=="fr")?"selected":""; ?>>France</option>
+                <option value="pl" <?= ( !empty($_SESSION["data"]) && $_SESSION["data"]["country"]=="pl")?"selected":""; ?>>Pologne</option>
+                <option value="al" <?= ( !empty($_SESSION["data"]) && $_SESSION["data"]["country"]=="al")?"selected":""; ?>>Algérie</option>
+                <option value="be" <?= ( !empty($_SESSION["data"]) && $_SESSION["data"]["country"]=="be")?"selected":""; ?>>Belgique</option>
                     </select></div>
-                <div class="champ">
-                    <input type="date" class="input-champ" name="birthday" required="required">
+                
+                    <div class="champ">
+                    <input type="date" class="input-champ" name="birthday" required="required"
+                    value="<?= ( !empty($_SESSION["data"]))?$_SESSION["data"]["birthday"]:""; ?>">
                 </div>
                 <div>
                     <input type="checkbox" class="form-check-input" id="cgu" name="cgu" required="required">
                     <label for="cgu" class="form-label">J'accepte les CGUs</label>
+                </div>
+                <div>
+                    <input type="checkbox" class="form-check-input" id="newsletter" name="newsletter">
+                    <label for="cgu" class="form-label">Je m'inscris aux newletter</label>
                 </div>
                 <button class="button3">Suivant</button>
             </form>
