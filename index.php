b03e1618e1eaea4345c33ace9abdb51a0baf5a68
Tiziana <95030727+TiziSil@users.noreply.github.com>
2023-06-02 23:45:59 +0200

    Debut de mise en place des cookies

--- index.php
+++ index.php
@@ -11,10 +11,19 @@ include "head.php"; ?>
 ?>
 
 <body>
+
     <div id="bouton-remonter">
         <a href="#top"><img src="assets/images/fleche-haut.png" alt="Remonter"></a>
     </div>
     <?php include "header.php"; ?>
+    <div class="cookies">
+        <h4 class="h4-cookies">Cookies</h4>
+        <a href=""><img src="assets/images/cookies.avif">
+        <p>Veuillez accepter les cookies avant de continuer</p>
+        <p><a href="#">En savoir plus</a></p>
+        <button class="button3 button-cookies">Accepter tous les cookies</button>
+        <button class="button3 button-cookies">Ne pas accepter les cookies</button>
+    </div>
     <?php include "rooting.php"; ?>
     <?php include "footer.php"; ?>
 </body>
