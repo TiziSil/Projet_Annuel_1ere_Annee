208fa9ffc4794a663f76b716b7bc01c348a75566
Tiziana <95030727+TiziSil@users.noreply.github.com>
2023-07-02 16:47:44 +0200

    Liste des topics de forums

--- pages/forum.php
+++ pages/forum.php
@@ -1,27 +1,11 @@
-<section class="forum py-4 d-flex flex-column">
-    <div class="container py-4">
+<section class="forum py-5 d-flex flex-column">
+    <div class="container py-5">
         <h1 class="h1-forum">Forum de discussion</h1>
-        <form id="" class="d-flex row" method="POST">
 
-            <div class="col-10 input-form-mon-compte d-flex">
-                <svg width="16px" height="16px">
-                    <image height="16px" fill="#DEC7B1" width="16px" href="./assets/images/loupe.svg" />
-                </svg>
-                <input class="input-form-mon-compte" placeholder="Recherchez une discussion" required type="text">
-            </div>
-            <button class="button2 col-2">Recherchez</button>
-        </form>
 
         <div class="forum-input boite d-flex flex-column">
             <h4>Créer un topic</h4>
-            <form method="POST">
-
-                <?php 
-                //     if (isset($errorMsg)) {
-                //         echo '<p>' . $errorMsg . '</p>';
-                // } elseif(isset($successMsg)) 
-                ?>
-
+            <form action="./core/forumAddTopic.php" method="POST">
                 <div class="d-flex mb-3">
                     <input class="form-control" type="text" placeholder="Titre de votre question" name="title">
                 </div>
@@ -31,56 +15,63 @@
                 </div>
                 <button type="submit" name="validate" class="button2 button-form-forum">Enregister</button>
             </form>
+        </div>
+
+    </div>
+</section>
+
+<section class="forum-section-2 py-4 d-flex flex-column">
+    <div class="container py-5 ">
+        <div clas="boite d-flex flex-column">
+            <form class="d-flex flex-row boite" method="POST">
+                <div class="col-8 barre-de-recherche-topic d-flex">
+                    <svg width="16px" height="16px">
+                        <image height="16px" fill="#DEC7B1" width="16px" href="./assets/images/loupe.svg" />
+                    </svg>
+                    <input class="barre-de-recherche-topic" placeholder="Recherchez une discussion" required type="text" name="recherche">
+                </div>
+                <button class="button2 col-2 mx-1">Recherchez</button>
+                <a href="./forum" class="button2 col-2">Supprimer critère</a>
+            </form>
+        </div>
+        <div class="forum-input boite d-flex flex-column">
+
             <?php
+            $connection = connectDB();
 
-            if (isset($_POST['validate'])) {
-                echo $_POST['title'];
-                echo $_POST['content'];
-                if (!empty($_POST['title']) and !empty($_POST["content"])) {
+            if(isset($_POST['recherche'])) {
+                $getAllMyQuestions = $connection->prepare('SELECT question_id, question_title, question_text, question_id_authorFROM MAKISINE_FORUM WHERE question_title LIKE ? ORDER BY question_id DESC');
+                $getAllMyQuestions->execute(array($_POST['recherche']));
+            } else {
+                $getAllMyQuestions = $connection->prepare('SELECT question_id, question_title, question_text, question_id_author FROM MAKISINE_FORUM ORDER BY question_id DESC');
+                $getAllMyQuestions->execute();
+            }
 
-                    $question_title = htmlspecialchars($_POST['title']);
-                    $question_text = nl2br(htmlspecialchars($_POST['content']));      // DataGrip -> MAKASINE_FORUM
-                    $question_date = date('Y/m/d');
-                    $question_id_author = 0;
-                    $question_pseudo_author = "Silrine";
 
-                    $connection = connectDB();
-                    $insertQuestionWebsite = $connection->prepare('INSERT INTO MAKISINE_FORUM(question_title, question_text, question_date, question_id_author, question_pseudo_author)VALUES(?,?,?,?,?)');
-                    $insertQuestionWebsite->execute(
-                        array(
-                            $question_title, 
-                            $question_text, 
-                            $question_date, 
-                            $question_id_author, 
-                            $question_pseudo_author
-                        )
-                    );
+            while ($question = $getAllMyQuestions->fetch()) {
+            ?>
+                <div class="card d-flex my-1">
+                    <div class="card-header d-flex flex-row justify-content-between">
+                        <h5 class="d-flex"><?php echo $question['question_title']; ?></h5>
 
-                    $successMsg = "Votre question a bien été publiée !";
-                    echo $successMsg;
+                        <p class="card-text liste-text-forum d-flex">
+                            <?php echo $question['question_text']; ?>
+                        </p>
 
-                } else {
-                    $errorMsg = "Veuillez compléter tous les champs";
-                    echo $errorMsg;
-                }
+                        <div class="buttons-liste-forum">
+                            <a href="#" class="button3">Accéder à l'article</a>
+                            <?php if($_SESSION['id'] === $question['question_id_author']) {?>
+                                <a href="#" class="button3">Modifier l'article</a>
+                                <a href="#" class="button3">Supprimer mon article</a>
+                            <?php } ?>
+
+                        </div>
+                    </div>
+                </div>
+            <?php
             }
             ?>
 
         </div>
-
-        <!-- 
-        <div class="forum-input boite d-flex flex-column">
-            <h4>Discussion</h4>
-            <div class="d-flex mb-3">
-                <input class="form-control" type="text" placeholder="Titre de votre question">
-            </div>
-
-            <div class="d-flex mb-3">
-                <textarea class="form-control" placeholder="Veuillez écrire ici"></textarea>
-            </div>
-            <button type="submit" class="button2 button-form-forum" name="validate">Enregister</button>
-
-        </div> -->
-
     </div>
 </section>
