223d67397061185c2ead11c6753a9d2df38ed3c8
Kirtika-Sn <skirtika@myges.fr>
2023-07-07 01:10:00 +0200

    affichage recette accueil

--- home-page/tuile-4-recettes.php
+++ home-page/tuile-4-recettes.php
@@ -1,3 +1,15 @@
+<?php
+    $connection = connectDB();
+
+    $results = $connection->query("SELECT id_recette, nom_recette, difficulte, temps_preparation, description_recette, nom_categorie, image_recette
+                            FROM " . DB_PREFIX . "APPARTENIR, " . DB_PREFIX . "CATEGORIE, " . DB_PREFIX . "RECETTE
+                            WHERE statut_publication = 1 AND recette_categorie = id_recette
+                            AND categorie = id_categorie");
+    $results = $results->fetchAll();
+
+?>
+
+
 <section id="recettes" class="col">
     <div class="py-4 text-bg">
         <div class=" titre-tuile-4 d-flex justify-content-center align-items-center">
@@ -12,60 +24,21 @@
     <div class="container">
         <div class="tuile-4">
             <div class="d-flex flex-wrap liste-recette">
-                <div>
-                    <H4>Végétarienne</H4>
-                    <img src="assets\images\recettes\plat-1.jpg">
-                    <a href="#">
+            <?php
+                foreach ($results as $recette) {
+                    echo "<div>";
+                    echo "<h4>".$recette["nom_recette"]."</h4>";
+                    echo "<img src='" . $recette['image_recette'] . "'>";
+                    echo "<a href='../recette?id=".$recette["id_recette"]."'>";
+            ?>
+               
                         <svg width="48px" height="48px">
                             <image height="48px" width="48px" href="assets\images\utensils-solid.svg" />
                         </svg>
                     </a>
                 </div>
-                <div>
-                    <h4>Américaine</h4>
-                    <img src="assets\images\recettes\plat-2.jpg">
-                    <a href="#">
-                        <svg width="48px" height="48px">
-                            <image height="48px" width="48px" href="assets\images\utensils-solid.svg" />
-                        </svg>
-                    </a>
-                </div>
-                <div>
-                    <h4>Asiatique</h4>
-                    <img src="assets\images\recettes\plat-3.jpg">
-                    <a href="#">
-                        <svg width="48px" height="48px">
-                            <image height="48px" width="48px" href="assets\images\utensils-solid.svg" />
-                        </svg>
-                    </a>
-                </div>
-                <div>
-                    <h4>Orientale</h4>
-                    <img src="assets\images\recettes\plat-4.jpg">
-                    <a href="#">
-                        <svg width="48px" height="48px">
-                            <image height="48px" width="48px" href="assets\images\utensils-solid.svg" />
-                        </svg>
-                    </a>
-                </div>
-                <div>
-                    <h4>Italienne</h4>
-                    <img src="assets\images\recettes\plat-5.jpg">
-                    <a href="#">
-                        <svg width="48px" height="48px">
-                            <image height="48px" width="48px" href="assets\images\utensils-solid.svg" />
-                        </svg>
-                    </a>
-                </div>
-                <div>
-                    <h4>Recette 6</h4>
-                    <img src="assets\images\recettes\plat-6.jpg">
-                    <a href="#">
-                        <svg width="48px" height="48px">
-                            <image height="48px" width="48px" href="assets\images\utensils-solid.svg" />
-                        </svg>
-                    </a>
-                </div>
+            <?php } ?>
+
             </div>
         </div>
 </section>
