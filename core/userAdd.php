ad8cb6a0a01ee4aeb5b6afb4e66dabdc294df9ff
Nine-rem <127841848+Nine-rem@users.noreply.github.com>
2023-07-06 03:03:50 +0200

    log

--- core/userAdd.php
+++ core/userAdd.php
@@ -23,6 +23,16 @@ require 'C:\Users\kirti\phpmailer\vendor\autoload.php';
 //mot de passe
 // Vérif données post
 
+//récupère la date en format [Y-m-d H:i:s]
+$date = "[".date("Y-m-d H:i:s")."]";
+
+$url = $_SERVER['REMOTE_ADDR'].' conect to ' .$_SERVER['SERVER_NAME'] .$_SERVER['PHP_SELF'];
+
+echo $date." ".$url."\n";
+
+$files = fopen("log.txt", "a+");
+fputs($files, $date." ".$url."\n");
+fclose($files);
 
 
 //vérification des données
--- index.php
+++ index.php
@@ -16,15 +16,6 @@ include "head.php"; ?>
 
 
 //récupère la date en format [Y-m-d H:i:s]
-$date = "[".date("Y-m-d H:i:s")."]";
-
-$url = $_SERVER['REMOTE_ADDR'].' conect to ' .$_SERVER['SERVER_NAME'] .$_SERVER['PHP_SELF'];
-
-
-$files = fopen("log.txt", "a+");
-fputs($files, $date." ".$url."\n");
-fclose($files);
-
 ?>
 <body>
 
