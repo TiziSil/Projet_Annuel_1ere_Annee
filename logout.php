1225c6286419f922acc0454a517b5a96f0f2331f
Nine-rem <127841848+Nine-rem@users.noreply.github.com>
2023-07-06 14:18:08 +0200

    logout repare

--- logout.php
+++ logout.php
@@ -1,10 +1,10 @@
-
 <?php
-session_start();				// On récupère la connexion existante sinon on ne peut pas la supprimer
+session_start();			
+require 'core/functions.php';	// On récupère la connexion existante sinon on ne peut pas la supprimer
 require "conf.inc.php";   		// On veut deconnecter l'utilisateur
 session_destroy();   			// On veut deconnecter l'utilisateur
 session_unset();   				// On veut deconnecter l'utilisateur
 unset($_SESSION['email']);  	// On veut deconnecter l'utilisateur
 unset($_SESSION['login']);  	// On veut deconnecter l'utilisateur
 $_SESSION = array();   			// On veut deconnecter l'utilisateur
-header("Location: .");   		// On veut deconnecter l'utilisateur
+redirection('index.php');   		// On veut deconnecter l'utilisateur
